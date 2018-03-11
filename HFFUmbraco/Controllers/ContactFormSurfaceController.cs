eb50400f8a2510982c45e23aef4cdf66e9bb7ebc
tom.swift <tom.swift@onthebeach.co.uk>
2018-03-11 17:29:16 +0000

    Display the working beta site

---
+++ HFFUmbraco/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Mail;
+using System.Web.Mvc;
+using HFFUmbraco.Models;
+using Umbraco.Web.Mvc;
+
+namespace HFFUmbraco.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        public ActionResult RenderContactForm()
+        {
+            return PartialView("~/Views/Partials/ContactFormSurface/ContactForm.cshtml", new ContactFormViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HandleContactForm(ContactFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+            MailMessage email = new MailMessage(model.Email, "tom@tintinet.co.uk");
+            email.Subject = "Contact Form Request";
+            email.Body = model.Message;
+            try
+            {
+                SmtpClient smtp = new SmtpClient();
+                smtp.Send(email);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            TempData["IsSuccessful"] = true;
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ HFFUmbraco/Models/ContactFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HFFUmbraco.Models
+{
+    public class ContactFormViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
